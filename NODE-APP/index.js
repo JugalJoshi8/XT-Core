a092bc6ef0e948df8afec3ff9bbbccb9bfb45fc5
Jugal <jkj.619@gmail.com>
2018-12-18 09:21:29 +0530

    changes

--- NODE-APP/index.js
+++ NODE-APP/index.js
@@ -10,9 +10,59 @@ eventEmitter.once('goodbye', _ => {
     console.log('I am hadling goodbye signal');
 });
 
+eventEmitter.on('add', function(a, b) {
+    console.log('sum', a + b);
+});
+
+eventEmitter.on('myevent', function() {
+    console.log(this);
+});
+
+eventEmitter.on('myevent', _ => {
+    console.log(this);
+});
+
+eventEmitter.on('asyncevent', _ => {
+    setImmediate(_ => {
+        console.log('asyncevent listner');
+    });
+});
+
 //emission
 
 eventEmitter.emit('greet');
 eventEmitter.emit('greet');
 eventEmitter.emit('goodbye');
 eventEmitter.emit('goodbye');
+eventEmitter.emit('add', 20, 34);
+eventEmitter.emit('asyncevent');
+eventEmitter.emit('myevent');
+eventEmitter.emit('myevent');
+
+// extending event emitter
+class Room extends EventEmitter {
+    constructor() {
+        super();
+        this.on('initialize', _ => {
+            console.log('light initialized;')
+        });
+        this.on('destroy', _ => {
+            console.log('light closed;')
+        });
+    }
+
+    open() {
+        this.emit('initialize')
+    }
+
+    close() {
+        this.emit('destroy');
+    }
+}
+
+const room = new Room();
+room.open();
+room.close();
+
+
+
